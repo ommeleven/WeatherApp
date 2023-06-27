6d5476ad07742f91a99e9efaebbe5600381f7e05
ommeleven <dangeom94@gmail.com>
2023-06-27 17:50:48 -0500

    static api call

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,6 +1,12 @@
 import requests
-apiKey = '36bab58ca3e8543ac064974051023e67'
-cityName = 'Chicago'
-countryCode = 'US'
-res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}")
-print(res.json())
+
+apiKey = "36bab58ca3e8543ac064974051023e67"
+cityName = "Chicago"
+countryCode = "US"
+res = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}&units=imperial"
+)
+currTemp = res.json()["main"]["temp"]
+print()
+if currTemp > 60:
+    print("don't carry a jacket")
