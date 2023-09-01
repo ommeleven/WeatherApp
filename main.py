b190b5fd8a7bfecc4e77728ecc581e9121e1157b
ommeleven <dangeom94@gmail.com>
2023-08-31 22:12:51 -0500

    created new files

---
+++ main.py
@@ -0,0 +1,22 @@
+class Weather:
+    def __init__(self):
+        self.temperature = 0.0
+        self.description = ""
+        self.icon = ""
+        self.humidity = 0
+        self.wind_speed = 0.0
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature
+
+    def set_description(self, description):
+        self.description = description
+
+    def set_icon(self, icon):
+        self.icon = icon
+
+    def set_humidity(self, humidity):
+        self.humidity = humidity
+
+    def set_wind_speed(self, wind_speed):
+        self.wind_speed = wind_speed
