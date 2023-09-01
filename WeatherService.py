56bb98b623cf557d7e0de668931715c3a8831c18
ommeleven <dangeom94@gmail.com>
2023-08-31 22:29:03 -0500

    updated all files basic control

--- WeatherService.py
+++ WeatherService.py
@@ -1,14 +1,8 @@
-import Forecast, Weather  # noqa: E401
-
-
 class WeatherService:
-    def __init__(self):
-        self.api_key = "36bab58ca3e8543ac064974051023e67"
+    def get_weather(self, location):
+        # Retrieve weather data from the weather API using location coordinates
+        pass
 
-    def get_weather(self):
-        # API call and retrieval logic to get the current weather
-        return Weather()
-
-    def get_forecast(self):
-        # API call and retrieval logic to get the weather forecast
-        return [Forecast(), Forecast()]
+    def get_cached_weather(self):
+        # Retrieve weather data from the cache
+        pass
