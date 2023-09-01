b7e2c030d6c6d6afd92c38be6fd6fd2f6a942419
ommeleven <dangeom94@gmail.com>
2023-08-31 23:36:28 -0500

    making API call #2

--- weatherAPI.py
+++ weatherAPI.py
@@ -3,22 +3,27 @@ import requests
 apiKey = "36bab58ca3e8543ac064974051023e67"
 cityName = "Chicago"
 countryCode = "US"
+url = "https://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}&units=imperial"
+
 
 
  
 class WeatherAPI:
+
     apiKey = "36bab58ca3e8543ac064974051023e67"
     cityName = input("Whih city do you live in? ")
     countryCode = "US"
     currWeather = ''
+    url = "https://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}&units=imperial"
+
     def get_weather_data(self, location):
         pass
         
-    def make_api_request(self, url):
+    def make_api_request(self):
         # network requests to external API
-        res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}&units=imperial")
+        res = requests.get(url=url)
         currWeather = res.json()
+        print(currWeather)
 
-    print(currWeather)
-
-WeatherAPI()
+APIcall = WeatherAPI()
+APIcall.make_api_request()
