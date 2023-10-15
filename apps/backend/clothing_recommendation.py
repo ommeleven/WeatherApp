b1544493304dd30a5601e6f1c63dc92ffb82f157
ommeleven <dangeom94@gmail.com>
2023-10-15 00:04:10 -0500

    building neural net

--- apps/backend/clothing_recommendation.py
+++ apps/backend/clothing_recommendation.py
@@ -1,4 +1,58 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+
+
 class ClothingRecommendation:
-    def generate_recommendations(self, weather):
-        # Generate clothing recommendations based on weather data
+    app = Flask(__name)
+
+    # Load the trained neural network model
+    model = tf.keras.models.load_model("clothing_recommendation_model.h5")
+
+    # Define a mapping of weather conditions to class labels
+    weather_conditions = {
+        "Clear": 0,
+        "Rain": 1,
+        "Snow": 2,
+        "Clouds": 3,
+        "Thunderstorm": 4,
+        "Fog": 5,
+    }
+
+    @app.route("/recommend_clothing", methods=["POST"])
+    def recommend_clothing(self):
+        data = request.get_json()
+
+        # Extract relevant weather information from the request
+        temperature = data["temperature"]
+        humidity = data["humidity"]
+        weather_type = data["weather_type"]
+
+        # Map the weather type to a class label
+        class_label = self.weather_conditions.get(weather_type, -1)
+
+        if class_label == -1:
+            return jsonify(
+                {"recommendation": "No recommendation available for this weather."}
+            )
+
+        # Prepare the input data for the neural network
+        input_data = np.array([[temperature, humidity, class_label]])
+
+        # Make a prediction using the neural network
+        prediction = self.model.predict(input_data)
+
+        # Depending on your model and data, you might need to post-process the prediction
+
+        # Return clothing recommendation based on the prediction
+        return jsonify(
+            {"recommendation": "Wear " + self.get_recommendation(prediction)}
+        )
+
+    def get_recommendation(self, prediction):
+        # Your logic to interpret the model's prediction and provide a clothing recommendation
+        # You can map class labels to clothing recommendations
         pass
+
+    if __name__ == "__main":
+        app.run(debug=True)
